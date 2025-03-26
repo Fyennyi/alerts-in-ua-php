a97b7aaf4fb10fd131d490837c040a644f134792
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-03-26 08:43:41 +0200

    Create Alerts.php

---
+++ src/Alerts.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace AlertsUA;
+
+class Alerts
+{
+    private $alerts;
+
+    private $last_updated_at;
+
+    private $disclaimer;
+
+    public function __construct($data)
+    {
+        $this->alerts = array_map(fn ($alert) => new Alert($alert), $data['alerts']);
+        $meta = $data['meta'] ?? [];
+        $this->last_updated_at = UaDateParser::parseDate($meta['last_updated_at'] ?? null, '%Y/%m/%d %H:%M:%S %z');
+        $this->disclaimer = $data['disclaimer'] ?? '';
+    }
+
+    public function filter(...$args)
+    {
+        $filtered_alerts = $this->alerts;
+        for ($i = 0; $i < count($args); $i += 2) {
+            $filtered_alerts = array_filter($filtered_alerts, fn ($alert) => $alert->{$args[$i]} == $args[$i + 1]);
+        }
+
+        return $filtered_alerts;
+    }
+
+    public function getOblastAlerts()
+    {
+        return $this->getAlertsByAlertType('oblast');
+    }
+
+    public function getRaionAlerts()
+    {
+        return $this->getAlertsByAlertType('raion');
+    }
+
+    public function getHromadaAlerts()
+    {
+        return $this->getAlertsByAlertType('hromada');
+    }
+
+    public function getCityAlerts()
+    {
+        return $this->getAlertsByAlertType('city');
+    }
+
+    public function getAlertsByAlertType($alert_type)
+    {
+        return $this->filter('alert_type', $alert_type);
+    }
+
+    public function getAlertsByLocationTitle($location_title)
+    {
+        return $this->filter('location_title', $location_title);
+    }
+
+    public function getAlertsByLocationType($location_type)
+    {
+        return $this->filter('location_type', $location_type);
+    }
+
+    public function getAlertsByOblast($oblast_title)
+    {
+        return $this->filter('location_oblast', $oblast_title);
+    }
+
+    public function getAlertsByOblastUid($oblast_uid)
+    {
+        return $this->filter('location_oblast_uid', $oblast_uid);
+    }
+
+    public function getAlertsByLocationUid($location_uid)
+    {
+        return $this->filter('location_uid', $location_uid);
+    }
+
+    public function getAirRaidAlerts()
+    {
+        return $this->getAlertsByAlertType('air_raid');
+    }
+
+    public function getArtilleryShellingAlerts()
+    {
+        return $this->getAlertsByAlertType('artillery_shelling');
+    }
+
+    public function getUrbanFightsAlerts()
+    {
+        return $this->getAlertsByAlertType('urban_fights');
+    }
+
+    public function getNuclearAlerts()
+    {
+        return $this->getAlertsByAlertType('nuclear');
+    }
+
+    public function getChemicalAlerts()
+    {
+        return $this->getAlertsByAlertType('chemical');
+    }
+
+    public function getAllAlerts()
+    {
+        return $this->alerts;
+    }
+
+    public function getLastUpdatedAt()
+    {
+        return $this->last_updated_at;
+    }
+
+    public function getDisclaimer()
+    {
+        return $this->disclaimer;
+    }
+
+    public function __iter()
+    {
+        return new \ArrayIterator($this->alerts);
+    }
+
+    public function __repr()
+    {
+        return json_encode($this->alerts);
+    }
+
+    public function __len()
+    {
+        return count($this->alerts);
+    }
+}
