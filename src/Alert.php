d213d82130a55a93e9a352ac273d417835a74571
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-04-14 07:49:50 +0300

    refactor(model): Make Alert constructor properties nullable and add default values

--- src/Alert.php
+++ src/Alert.php
@@ -32,19 +32,19 @@ class Alert
 
     public function __construct($data)
     {
-        $this->id = $data['id'];
-        $this->location_title = $data['location_title'];
-        $this->location_type = $data['location_type'];
-        $this->started_at = UaDateParser::parseDate($data['started_at']);
-        $this->finished_at = UaDateParser::parseDate($data['finished_at']);
-        $this->updated_at = UaDateParser::parseDate($data['updated_at']);
-        $this->alert_type = $data['alert_type'];
-        $this->location_uid = $data['location_uid'];
-        $this->location_oblast = $data['location_oblast'];
-        $this->location_oblast_uid = $data['location_oblast_uid'];
-        $this->location_raion = $data['location_raion'];
-        $this->notes = $data['notes'];
-        $this->calculated = $data['calculated'];
+        $this->id = $data['id'] ?? null;
+        $this->location_title = $data['location_title'] ?? null;
+        $this->location_type = $data['location_type'] ?? null;
+        $this->started_at = UaDateParser::parseDate($data['started_at'] ?? null);
+        $this->finished_at = UaDateParser::parseDate($data['finished_at'] ?? null);
+        $this->updated_at = UaDateParser::parseDate($data['updated_at'] ?? null);
+        $this->alert_type = $data['alert_type'] ?? null;
+        $this->location_uid = $data['location_uid'] ?? null;
+        $this->location_oblast = $data['location_oblast'] ?? null;
+        $this->location_oblast_uid = $data['location_oblast_uid'] ?? null;
+        $this->location_raion = $data['location_raion'] ?? null;
+        $this->notes = $data['notes'] ?? null;
+        $this->calculated = $data['calculated'] ?? false;
     }
 
     public function isFinished()
