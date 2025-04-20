d8988f8de90920dcc65f44db2decce82a3f6eb09
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-04-21 00:25:20 +0300

    feat(tests): Add UaDateParserTest

---
+++ tests/Unit/UaDateParserTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use AlertsUA\UaDateParser;
+
+class UaDateParserTest extends TestCase
+{
+    public function testParseValidISO8601Date()
+    {
+        $dateString = '2023-01-02T10:15:30.000Z';
+        $dateTime = UaDateParser::parseDate($dateString);
+        
+        $this->assertInstanceOf(DateTime::class, $dateTime);
+        $this->assertEquals('Europe/Kyiv', $dateTime->getTimezone()->getName());
+        
+        // Since Kyiv is UTC+2/UTC+3, the parsed time will be later than UTC
+        $this->assertGreaterThanOrEqual('10:15:30', $dateTime->format('H:i:s'));
+    }
+
+    public function testParseNullDate()
+    {
+        $this->assertNull(UaDateParser::parseDate(null));
+        $this->assertNull(UaDateParser::parseDate(''));
+    }
+
+    public function testParseVariousDateFormats()
+    {
+        $formats = [
+            '2023/01/02 10:15:30',
+            '2023-01-02 10:15:30',
+            '2023-01-02T10:15:30',
+            '2023-01-02T10:15:30.000',
+        ];
+
+        foreach ($formats as $format) {
+            $dateTime = UaDateParser::parseDate($format);
+            $this->assertInstanceOf(DateTime::class, $dateTime);
+        }
+    }
+}
