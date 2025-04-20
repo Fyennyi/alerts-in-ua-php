9a7afb99509105856f87d7452ab80ab95008d5b8
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-04-21 00:28:27 +0300

    Create AirRaidAlertOblastStatusesTest.php

---
+++ tests/Unit/AirRaidAlertOblastStatusesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use AlertsUA\AirRaidAlertOblastStatuses;
+
+class AirRaidAlertOblastStatusesTest extends TestCase
+{
+    public function testStatusesWithOblastLevelOnly()
+    {
+        $data = 'ANBCAD'; // Simulated status data for 6 oblasts
+        $statuses = new AirRaidAlertOblastStatuses($data, true);
+        
+        // Since oblast_level_only is true, only 'A' statuses should be included
+        $this->assertCount(2, $statuses->getStatuses());
+        
+        foreach ($statuses->getStatuses() as $status) {
+            $this->assertInstanceOf(AirRaidAlertOblastStatus::class, $status);
+            $this->assertEquals('A', $status->getStatus());
+        }
+    }
+
+    public function testStatusesWithAllLevels()
+    {
+        $data = 'ANBCAD'; // Simulated status data for 6 oblasts
+        $statuses = new AirRaidAlertOblastStatuses($data, false);
+        
+        // All statuses should be included
+        $this->assertCount(6, $statuses->getStatuses());
+    }
+}
