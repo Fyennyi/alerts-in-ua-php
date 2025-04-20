e42877f769e3dc7fe7be95c2e993708588dc40b1
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-04-21 00:30:10 +0300

    feat(tests): Add MockAlertTest for AirRaidAlertOblastStatus

---
+++ tests/Unit/MockAlertTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use AlertsUA\AirRaidAlertOblastStatuses;
+
+class MockAlertTest extends TestCase
+{
+    public function testAirRaidAlertOblastStatus()
+    {
+        $oblast = 'м. Київ';
+        $status = 'A';
+        
+        $alertStatus = new AirRaidAlertOblastStatus($oblast, $status);
+        
+        $this->assertEquals($oblast, $alertStatus->getOblast());
+        $this->assertEquals($status, $alertStatus->getStatus());
+    }
+}
