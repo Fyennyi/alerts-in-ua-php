90904625dcce242b829d3d2388a4da3ae23307fb
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-04-21 00:26:18 +0300

    feat(tests): Add LocationUidResolverTest

---
+++ tests/Unit/LocationUidResolverTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use AlertsUA\LocationUidResolver;
+
+class LocationUidResolverTest extends TestCase
+{
+    public function testResolveUid()
+    {
+        $resolver = new LocationUidResolver();
+        
+        $this->assertEquals(31, $resolver->resolveUid('м. Київ'));
+        $this->assertEquals(22, $resolver->resolveUid('Харківська область'));
+        $this->assertEquals('Unknown UID', $resolver->resolveUid('Неіснуюча область'));
+    }
+
+    public function testResolveLocationTitle()
+    {
+        $resolver = new LocationUidResolver();
+        
+        $this->assertEquals('м. Київ', $resolver->resolveLocationTitle(31));
+        $this->assertEquals('Харківська область', $resolver->resolveLocationTitle(22));
+        $this->assertEquals('Unknown location', $resolver->resolveLocationTitle(999));
+    }
+}
