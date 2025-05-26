883f9340b1235eb86bb9b893a8b5da3d1cb5110d
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-05-27 00:46:20 +0300

    docs(util): Add PHPDoc to getUserAgent method in UserAgent

--- src/UserAgent.php
+++ src/UserAgent.php
@@ -6,6 +6,11 @@ class UserAgent
 {
     private const DEFAULT_AGENT = 'aiu-php-client/1.0 (+https://alerts.in.ua)';
 
+    /**
+     * Get User-Agent string for API requests
+     *
+     * @return string User-Agent string from environment variable or default value
+     */
     public static function getUserAgent() : string
     {
         return getenv('AIU_USER_AGENT') ?: self::DEFAULT_AGENT;
