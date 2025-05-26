6bffe0338c5cd8b5b1d8b68201b28deeb08503b4
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-05-27 00:27:19 +0300

    Update AirRaidAlertOblastStatus.php

--- src/AirRaidAlertOblastStatus.php
+++ src/AirRaidAlertOblastStatus.php
@@ -8,17 +8,33 @@ class AirRaidAlertOblastStatus
 
     private $status;
 
+    /**
+     * Constructor for AirRaidAlertOblastStatus
+     *
+     * @param  string  $oblast  Oblast name
+     * @param  string  $status  Alert status code
+     */
     public function __construct($oblast, $status)
     {
         $this->oblast = $oblast;
         $this->status = $status;
     }
 
+    /**
+     * Get oblast name
+     *
+     * @return string Oblast name
+     */
     public function getOblast()
     {
         return $this->oblast;
     }
 
+    /**
+     * Get alert status
+     *
+     * @return string Alert status code
+     */
     public function getStatus()
     {
         return $this->status;
