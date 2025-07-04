98619088da279926c570b82f0eca8f403a325b80
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-07-04 23:21:31 +0300

    style(tests): Remove blank lines for code cleanup

--- tests/Unit/AirRaidAlertOblastStatusesTest.php
+++ tests/Unit/AirRaidAlertOblastStatusesTest.php
@@ -12,10 +12,10 @@ class AirRaidAlertOblastStatusesTest extends TestCase
     {
         $data = 'ANBCAD'; // Simulated status data for 6 oblasts
         $statuses = new AirRaidAlertOblastStatuses($data, true);
-        
+
         // Since oblast_level_only is true, only 'A' statuses should be included
         $this->assertCount(2, $statuses->getStatuses());
-        
+
         foreach ($statuses->getStatuses() as $status) {
             $this->assertInstanceOf(AirRaidAlertOblastStatus::class, $status);
             $this->assertEquals('A', $status->getStatus());
@@ -26,7 +26,7 @@ class AirRaidAlertOblastStatusesTest extends TestCase
     {
         $data = 'ANBCAD'; // Simulated status data for 6 oblasts
         $statuses = new AirRaidAlertOblastStatuses($data, false);
-        
+
         // All statuses should be included
         $this->assertCount(6, $statuses->getStatuses());
     }
