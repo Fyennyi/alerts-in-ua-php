ddb788d2afd5a8c68d9ffd7c185b197dc31c144c
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-07-06 13:07:29 +0300

    refactor(model): Enhance data validation and type safety in Alert constructor

--- src/Model/Alert.php
+++ src/Model/Alert.php
@@ -39,18 +39,18 @@ class Alert
      */
     public function __construct(array $data)
     {
-        $this->id = $data['id'] ?? 0;
-        $this->location_title = $data['location_title'] ?? null;
-        $this->location_type = $data['location_type'] ?? null;
-        $this->started_at = UaDateParser::parseDate($data['started_at'] ?? null);
-        $this->finished_at = UaDateParser::parseDate($data['finished_at'] ?? null);
-        $this->updated_at = UaDateParser::parseDate($data['updated_at'] ?? null);
-        $this->alert_type = $data['alert_type'] ?? null;
-        $this->location_uid = isset($data['location_uid']) ? (int) $data['location_uid'] : null;
-        $this->location_oblast = $data['location_oblast'] ?? null;
-        $this->location_oblast_uid = isset($data['location_oblast_uid']) ? (int) $data['location_oblast_uid'] : null;
-        $this->location_raion = $data['location_raion'] ?? null;
-        $this->notes = $data['notes'] ?? null;
+        $this->id = isset($data['id']) && is_int($data['id']) ? $data['id'] : 0;
+        $this->location_title = isset($data['location_title']) && is_string($data['location_title']) ? $data['location_title'] : '';
+        $this->location_type = isset($data['location_type']) && is_string($data['location_type']) ? $data['location_type'] : null;
+        $this->started_at = isset($data['started_at']) && is_string($data['started_at']) ? UaDateParser::parseDate($data['started_at']) : null;
+        $this->finished_at = isset($data['finished_at']) && is_string($data['finished_at']) ? UaDateParser::parseDate($data['finished_at']) : null;
+        $this->updated_at = isset($data['updated_at']) && is_string($data['updated_at']) ? UaDateParser::parseDate($data['updated_at']) : null;
+        $this->alert_type = isset($data['alert_type']) && is_string($data['alert_type']) ? $data['alert_type'] : null;
+        $this->location_uid = isset($data['location_uid']) && (is_int($data['location_uid']) || is_string($data['location_uid'])) ? (int) $data['location_uid'] : null;
+        $this->location_oblast = isset($data['location_oblast']) && is_string($data['location_oblast']) ? $data['location_oblast'] : null;
+        $this->location_oblast_uid = isset($data['location_oblast_uid']) && (is_int($data['location_oblast_uid']) || is_string($data['location_oblast_uid'])) ? (int) $data['location_oblast_uid'] : null;
+        $this->location_raion = isset($data['location_raion']) && is_string($data['location_raion']) ? $data['location_raion'] : null;
+        $this->notes = isset($data['notes']) && is_string($data['notes']) ? $data['notes'] : null;
         $this->calculated = isset($data['calculated']) ? (bool) $data['calculated'] : false;
     }
 
