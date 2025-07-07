0dc3c13d59b2dec4a1ba036935b8a183c44814d4
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-07-07 07:40:22 +0300

    refactor(test): Use getter methods in AlertTest for consistency

--- tests/Unit/AlertTest.php
+++ tests/Unit/AlertTest.php
@@ -28,18 +28,18 @@ class AlertTest extends TestCase
 
         $alert = new Alert($data);
 
-        $this->assertEquals(123, $alert->id);
-        $this->assertEquals('Київ', $alert->location_title);
-        $this->assertEquals('city', $alert->location_type);
-        $this->assertEquals('air_raid', $alert->alert_type);
-        $this->assertEquals(31, $alert->location_uid);
-        $this->assertEquals('м. Київ', $alert->location_oblast);
-        $this->assertEquals(31, $alert->location_oblast_uid);
-        $this->assertEquals('Test alert', $alert->notes);
-        $this->assertFalse($alert->calculated);
-        $this->assertInstanceOf(DateTime::class, $alert->started_at);
-        $this->assertInstanceOf(DateTime::class, $alert->finished_at);
-        $this->assertInstanceOf(DateTime::class, $alert->updated_at);
+        $this->assertEquals(123, $alert->getId());
+        $this->assertEquals('Київ', $alert->getLocationTitle());
+        $this->assertEquals('city', $alert->getLocationType());
+        $this->assertEquals('air_raid', $alert->getAlertType);
+        $this->assertEquals(31, $alert->getLocationUid());
+        $this->assertEquals('м. Київ', $alert->getLocationOblast());
+        $this->assertEquals(31, $alert->getLocationOblastUid());
+        $this->assertEquals('Test alert', $alert->getNotes());
+        $this->assertFalse($alert->isCalculated());
+        $this->assertInstanceOf(DateTime::class, $alert->getStartedAt());
+        $this->assertInstanceOf(DateTime::class, $alert->getFinishedAt());
+        $this->assertInstanceOf(DateTime::class, $alert->getUpdatedAt());
         $this->assertTrue($alert->isFinished());
     }
 
@@ -53,9 +53,9 @@ class AlertTest extends TestCase
 
         $alert = new Alert($data);
 
-        $this->assertEquals(123, $alert->id);
-        $this->assertEquals('Київ', $alert->location_title);
-        $this->assertNull($alert->finished_at);
+        $this->assertEquals(123, $alert->getId());
+        $this->assertEquals('Київ', $alert->getLocationTitle());
+        $this->assertNull($alert->getFinishedAt());
         $this->assertFalse($alert->isFinished());
     }
 }
