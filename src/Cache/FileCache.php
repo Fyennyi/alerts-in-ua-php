8ddad3a68a0c7ffab6c641e5496c00124895ebce
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-07-14 13:22:55 +0300

    refactor(cache): Improve caller file determination in FileCache constructor

--- src/Cache/FileCache.php
+++ src/Cache/FileCache.php
@@ -13,9 +13,13 @@ class FileCache implements ExpirableCacheInterface
     {
         if (null === $cache_dir) {
             $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
-            $caller_file = isset($backtrace[0]['file']) ? $backtrace[0]['file'] : getcwd();
-            $base_dir = dirname($caller_file);
+            $caller_file = $backtrace[0]['file'] ?? null;
 
+            if (! is_string($caller_file)) {
+                $caller_file = getcwd();
+            }
+
+            $base_dir = dirname($caller_file);
             $cache_dir = $base_dir . '/tmp/alerts_cache';
         }
 
