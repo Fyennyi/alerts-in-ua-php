896321b1bd7ac520d7f7a6ec81b77c5790bfdb23
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-07-16 15:50:58 +0300

    style(exception): Add empty line in RateLimitError class

--- src/Exception/RateLimitError.php
+++ src/Exception/RateLimitError.php
@@ -5,4 +5,6 @@ namespace Fyennyi\AlertsInUa\Exception;
 /**
  * Error thrown when too many requests are made to the API
  */
-class RateLimitError extends ApiError {}
+class RateLimitError extends ApiError
+{
+}
