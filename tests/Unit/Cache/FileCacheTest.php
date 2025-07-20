9ae764937a427bfa019f0fa34c8ea68c05ad6119
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-07-20 14:09:50 +0300

    feat(cache): Add FileCache unit tests

---
+++ tests/Unit/Cache/FileCacheTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Fyennyi\AlertsInUa\Tests\Unit\Cache;
+
+use Fyennyi\AlertsInUa\Cache\FileCache;
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+
+class FileCacheTest extends TestCase
+{
+    private $root;
+
+    private $cacheDir;
+
+    private FileCache $cache;
+
+    protected function setUp() : void
+    {
+        $this->root = vfsStream::setup('cache');
+        $this->cacheDir = $this->root->url();
+        $this->cache = new FileCache($this->cacheDir);
+    }
+
+    public function testSetAndGet()
+    {
+        $this->assertTrue($this->cache->set('key1', 'value1', 60));
+        $this->assertEquals('value1', $this->cache->get('key1'));
+    }
+
+    public function testGetNonExistent()
+    {
+        $this->assertNull($this->cache->get('non_existent_key'));
+    }
+
+    public function testGetExpired()
+    {
+        $this->cache->set('key_expired', 'value_expired', -1);
+        $this->assertNull($this->cache->get('key_expired'));
+        // Assert that the file still exists
+        $this->assertTrue($this->root->hasChild(md5('key_expired') . '.cache'));
+    }
+
+    public function testDelete()
+    {
+        $this->cache->set('key_to_delete', 'value_to_delete');
+        $this->assertTrue($this->cache->delete('key_to_delete'));
+        $this->assertNull($this->cache->get('key_to_delete'));
+    }
+
+    public function testClear()
+    {
+        $this->cache->set('key1', 'value1');
+        $this->cache->set('key2', 'value2');
+        $this->assertTrue($this->cache->clear());
+        $this->assertNull($this->cache->get('key1'));
+        $this->assertNull($this->cache->get('key2'));
+        $this->assertCount(0, $this->root->getChildren());
+    }
+
+    public function testHas()
+    {
+        $this->cache->set('key_has', 'value_has');
+        $this->assertTrue($this->cache->has('key_has'));
+        $this->assertFalse($this->cache->has('non_existent_key'));
+    }
+
+    public function testKeys()
+    {
+        $this->cache->set('key1', 'value1');
+        $this->cache->set('key2', 'value2');
+        $keys = $this->cache->keys();
+        $this->assertCount(2, $keys);
+        $this->assertContains('key1', $keys);
+        $this->assertContains('key2', $keys);
+    }
+
+    public function testCleanupExpired()
+    {
+        $this->cache->set('key_valid', 'value_valid', 3600);
+        $this->cache->set('key_expired', 'value_expired', -1); // Expired
+
+        $this->cache->cleanupExpired();
+
+        $this->assertTrue($this->cache->has('key_valid'));
+        $this->assertFalse($this->root->hasChild(md5('key_expired') . '.cache'));
+    }
+    
+    public function testGetStale()
+    {
+        $this->cache->set('key_stale', 'value_stale', -1); // Expired
+        $this->assertEquals('value_stale', $this->cache->getStale('key_stale'));
+    }
+}
