4078d4c4cfc7c9bc51de8bb2cec2d001ada3c803
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-07-20 15:01:34 +0300

    test(util): Add comprehensive unit tests for UaDateParser

--- tests/Unit/UaDateParserTest.php
+++ tests/Unit/UaDateParserTest.php
@@ -37,7 +37,34 @@ class UaDateParserTest extends TestCase
 
         foreach ($formats as $format) {
             $dateTime = UaDateParser::parseDate($format);
-            $this->assertInstanceOf(DateTime::class, $dateTime);
+            $this->assertInstanceOf(DateTime::class, $dateTime, "Failed to parse format: {$format}");
         }
+    }
+
+    public function testParseDateWithTimezone()
+    {
+        $dateString = '2023-07-20 12:00:00+03:00';
+        $dateTime = UaDateParser::parseDate($dateString);
+        $this->assertInstanceOf(DateTime::class, $dateTime);
+        $this->assertEquals('12:00:00', $dateTime->format('H:i:s'));
+        $this->assertEquals('Europe/Kyiv', $dateTime->getTimezone()->getName());
+    }
+
+    public function testParseDateWithStrtotimeFallback()
+    {
+        // This format is not in the list, so it should fall back to strtotime
+        $dateString = '20 July 2025 10:00:00 UTC';
+        $dateTime = UaDateParser::parseDate($dateString);
+        $this->assertInstanceOf(DateTime::class, $dateTime);
+        $this->assertEquals('13:00:00', $dateTime->format('H:i:s')); // Kyiv is UTC+3 in July
+    }
+
+    public function testParseInvalidDate()
+    {
+        $this->assertNull(UaDateParser::parseDate('not a date'));
+        // strtotime('2023-02-30') results in a valid date (2023-03-02), so we check for warnings
+        UaDateParser::parseDate('2023-02-30 10:00:00');
+        $errors = DateTime::getLastErrors();
+        $this->assertGreaterThan(0, $errors['warning_count']);
     }
 }
