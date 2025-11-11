db38ea596deb7e6a72ef8b4edc07414c187f2833
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-11 12:57:25 +0200

    chore(tests): Update expected User-Agent version to 0.3.0

--- tests/Unit/Util/UserAgentTest.php
+++ tests/Unit/Util/UserAgentTest.php
@@ -39,7 +39,7 @@ class UserAgentTest extends TestCase
         // Ensure the environment variable is not set
         putenv('AIU_USER_AGENT');
 
-        $defaultAgent = 'alerts-in-ua-php/0.2.9 (+https://github.com/Fyennyi/alerts-in-ua-php)';
+        $defaultAgent = 'alerts-in-ua-php/0.3.0 (+https://github.com/Fyennyi/alerts-in-ua-php)';
         $this->assertEquals($defaultAgent, UserAgent::getUserAgent());
     }
 }
