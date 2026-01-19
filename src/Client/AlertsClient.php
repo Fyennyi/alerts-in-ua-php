bc3b8d9746b0b7ff371b6d7725755b5c5254471a
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-19 20:13:12 +0200

    refactor: Remove unused NominatimGeoResolver import from AlertsClient

--- src/Client/AlertsClient.php
+++ src/Client/AlertsClient.php
@@ -41,7 +41,6 @@ use Fyennyi\AlertsInUa\Model\AirRaidAlertStatusResolver;
 use Fyennyi\AlertsInUa\Model\Alerts;
 use Fyennyi\AlertsInUa\Model\LocationUidResolver;
 use Fyennyi\AlertsInUa\Util\UserAgent;
-use Fyennyi\AlertsInUa\Util\NominatimGeoResolver;
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\RequestException;
