eb793bfb5475c8fe669086abcbbfe5b96d39f132
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-19 17:28:28 +0200

    feat: Add generate mapping script

---
+++ scripts/generate_mapping.php
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Fyennyi\AlertsInUa\Util\MappingGenerator;
+
+$locationsPath = __DIR__ . '/../src/Model/locations.json';
+$outputPath = __DIR__ . '/../src/Model/name_mapping.json';
+
+try {
+    $generator = new MappingGenerator($locationsPath, $outputPath);
+    $generator->generate();
+} catch (\Throwable $e) {
+    echo "Error: " . $e->getMessage() . "\n";
+    exit(1);
+}
